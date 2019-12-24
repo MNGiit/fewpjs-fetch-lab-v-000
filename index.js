842d11eb7d45622ed36fc72ca1dc0895c0cb3709
Mohammed Noori <kmanoorib@gmail.com>
2019-12-24 04:16:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function fetchBooks() {
   fetch("https://anapioficeandfire.com/api/books")
   .then(function(response) {
     return response.json();
-  }).then(function(json) {
+  })
+  .then(function(json) {
     // console.log(json)
     renderBooks(json)
   });
